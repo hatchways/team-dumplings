31f4df58595d80a3d7194e972b2c034639921b62
abdulraim <abdulrahimcisco92@gmail.com>
2021-10-13 07:11:35 +0300

    adding availability and timezone to profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: string,
+    gender: {
+        type: String,
+        required: true,
+        enum: ['Male', 'Female']
+    },
+    birthDate: Date,
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: Number,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
