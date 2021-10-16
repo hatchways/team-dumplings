e61dbb781213e3c1dd5db4602ccb2f4e48ed1190
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-16 11:31:33 +0100

    refactor code

--- server/app.js
+++ server/app.js
@@ -12,6 +12,8 @@ const logger = require("morgan");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const dogRouter = require("./routes/dog");
 
 const { json, urlencoded } = express;
 
@@ -45,6 +47,9 @@ app.use((req, res, next) => {
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/profile", profileRouter);
+
+app.use("/requests", requestRouter);
+app.use("/dogs", dogRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
