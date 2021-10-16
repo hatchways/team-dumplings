8c9f7137f97e240575ac47b20da9cd6f8bc70e87
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-16 22:28:51 +0100

    invoke the common middleware validator in every route with `req.params`.id in path `/:id`

--- server/routes/dog.js
+++ server/routes/dog.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { validateCreateUpdateDog } = require("../validators/dog");
+const { validateId } = require("../validators/common");
 const protect = require("../middleware/auth");
 const {
   loadDog,
@@ -12,12 +13,14 @@ const {
 
 router.route("/").get(protect, listDogs);
 
-router.route("/:id").get(protect, loadDog);
+router.route("/:id").get(validateId, protect, loadDog);
 
 router.route("/").post(protect, validateCreateUpdateDog, createDog);
 
-router.route("/:id").delete(protect, deleteDog);
+router.route("/:id").delete(validateId, protect, deleteDog);
 
-router.route("/:id").patch(protect, validateCreateUpdateDog, updateDog);
+router
+  .route("/:id")
+  .patch(validateId, protect, validateCreateUpdateDog, updateDog);
 
 module.exports = router;
