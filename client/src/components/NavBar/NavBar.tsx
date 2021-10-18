4105e95c18070b3136a6c13319c605b8ee8c4f37
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-18 14:45:50 +0100

    subscribe to `AuthContext` instead of using isActive, integrate `AuthMenu` && `NavMenu`

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,66 +1,24 @@
-import { AppBar, Avatar, Badge, Box, Button, Paper, Toolbar, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { AppBar, CircularProgress, Paper, Toolbar, Typography } from '@material-ui/core';
+import { useAuth } from '../../context/useAuthContext';
+import AuthMenu from './AuthMenu';
+import NavMenu from './NavMenu';
 import useStyles from './useStyles';
 
-interface IProps {
-  isActive: boolean;
-}
+export default function NavBar(): JSX.Element {
+  const { loggedInUser } = useAuth();
+  const { toolbar, leftLogoContainer, leftLogoText } = useStyles();
 
-const NavBar: React.FC<IProps> = ({ isActive }) => {
-  const classes = useStyles();
   return (
-    <>
-      <AppBar position="fixed">
-        <Toolbar className={classes.toolbar}>
-          <Paper variant="outlined" className={classes.leftLogoContainer}>
-            <img src="/assets/logo.png" className={classes.leftLogoImg} />
-            <Typography variant="h4" className={classes.leftLogoText}>
-              LovingSitter.
-            </Typography>
-          </Paper>
-
-          {!isActive && (
-            <Box className={classes.buttonContainer}>
-              <Link to={'/'} className={classes.link}>
-                <Button variant={'contained'} disableElevation className={classes.button + ' sitter'} disableRipple>
-                  BECOME A SIITTER
-                </Button>
-              </Link>
-              <Link to={'/'} className={classes.link}>
-                <Button variant={'contained'} disableElevation className={classes.button + ' login'} disableRipple>
-                  LOGIN
-                </Button>
-              </Link>
-              <Link to={'/'} className={classes.link}>
-                <Button variant={'contained'} disableElevation className={classes.button + ' signup'} disableRipple>
-                  SIGNUP
-                </Button>
-              </Link>
-            </Box>
-          )}
-          {isActive && (
-            <Box className={classes.buttonContainer}>
-              <Link to={'/sitters'} className={classes.link}>
-                <Button variant={'contained'} disableElevation className={classes.button + ' mySitters'} disableRipple>
-                  My Sitters
-                </Button>
-              </Link>
-              <Link to={'/messages'} className={classes.link}>
-                <Button variant={'contained'} disableElevation className={classes.button + ' messages'} disableRipple>
-                  <Badge classes={{ badge: classes.badge + ' active' }} variant="dot">
-                    Messages
-                  </Badge>
-                </Button>
-              </Link>
-              <Box className={classes.link}>
-                <Avatar alt="Hatch ways.." src="https://i.pravatar.cc/300" />
-              </Box>
-            </Box>
-          )}
-        </Toolbar>
-      </AppBar>
-    </>
+    <AppBar position="fixed">
+      <Toolbar className={toolbar}>
+        <Paper variant="outlined" className={leftLogoContainer}>
+          <img src="/assets/logo.png" />
+          <Typography variant="h4" className={leftLogoText}>
+            LovingSitter.
+          </Typography>
+        </Paper>
+        {loggedInUser ? <NavMenu /> : loggedInUser === undefined ? <CircularProgress /> : <AuthMenu />}
+      </Toolbar>
+    </AppBar>
   );
-};
-
-export default NavBar;
+}
