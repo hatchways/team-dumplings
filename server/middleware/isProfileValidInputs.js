68605caa65b3a0b0a7426b054ec877b8e35c3b5d
abdulraim <abdulrahimcisco92@gmail.com>
2021-10-19 21:10:31 +0300

    refactoring login to retrieve profileId

--- server/middleware/isProfileValidInputs.js
+++ server/middleware/isProfileValidInputs.js
@@ -1,6 +1,6 @@
 const isProfileValidInputs = (req, res, next) => {
     const inputs = Object.keys(req.body);
-    const allowedInputs = ['firstName', 'lastName', 'gender', 'phoneNumber', 'address', 'availability', 'description'];
+    const allowedInputs = ['firstName', 'lastName', 'gender', 'phoneNumber', 'address', 'availability', 'description', 'birthDate'];
     const isValidOperation = inputs.every((input) => allowedInputs.includes(input));
 
     if (!isValidOperation) {
