2a729e769e49b85afc0f2d77121614331e45d7d7
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-20 06:10:07 +0100

    `Login` style

--- client/src/pages/Login/useStyles.ts
+++ client/src/pages/Login/useStyles.ts
@@ -1,26 +1,22 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
     '& .MuiInput-underline:before': {
       borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
     },
-  },
-  authWrapper: {
-    display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    minHeight: '100vh',
-    paddingTop: 23,
+    alignContent: 'center',
+    justifyContent: 'center',
+    paddingTop: theme.spacing(10),
   },
   welcome: {
     fontSize: 26,
     paddingBottom: 20,
-    color: '#000000',
+    color: theme.palette.common.black,
     fontWeight: 700,
-    fontFamily: "'Open Sans'",
+    textAlign: 'center',
+    width: '95%',
   },
 }));
 
