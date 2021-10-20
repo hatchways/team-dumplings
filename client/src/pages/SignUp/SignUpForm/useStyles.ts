66be0e4df5eda0c7b84facd7c4dd7d6d300a1974
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-20 06:12:46 +0100

    `SignUp` JSX component Style

--- client/src/pages/SignUp/SignUpForm/useStyles.ts
+++ client/src/pages/SignUp/SignUpForm/useStyles.ts
@@ -1,34 +1,69 @@
+import { alpha } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(1),
+    paddingTop: theme.spacing(4),
+    '& .MuiFormControl-root': {
+      marginTop: theme.spacing(1),
+    },
+  },
+  caption: {
+    fontWeight: 'bold',
+  },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.primary.main,
   },
   label: {
-    fontSize: 19,
-    color: 'rgb(0,0,0,0.4)',
-    paddingLeft: '5px',
+    fontWeight: 800,
+    fontSize: 12,
+    color: theme.palette.common.black,
+    textTransform: 'uppercase',
+    marginTop: theme.spacing(1),
   },
   inputs: {
-    marginTop: '.8rem',
-    height: '2rem',
-    padding: '5px',
-  },
-  forgot: {
-    paddingRight: 10,
-    color: '#3a8dff',
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    width: '80%',
+    height: '100%',
+    padding: theme.spacing(2),
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
   },
   submit: {
-    margin: theme.spacing(3, 2, 2),
-    padding: 10,
+    textTransform: 'uppercase',
+    margin: theme.spacing(6, 2, 2),
+    padding: theme.spacing(1),
     width: 160,
     height: 56,
+    backgroundColor: theme.palette.primary.main,
     borderRadius: theme.shape.borderRadius,
-    marginTop: 49,
-    fontSize: 16,
-    backgroundColor: '#3a8dff',
-    fontWeight: 'bold',
+    fontSize: 12,
+    fontWeight: 600,
+  },
+  roleButton: {
+    marginTop: theme.spacing(2),
+    border: 1,
+    borderStyle: 'solid',
+    borderColor: theme.palette.primary.main,
+    background: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    '&.Mui-disabled': {
+      color: theme.palette.primary.main,
+      background: 'transparent',
+    },
+  },
+  circularProgress: {
+    color: theme.palette.common.white,
   },
 }));
 
