dc3bab3eebbb250b9d010d5baf9ef508bbd34392
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-20 06:06:05 +0100

    delete the become sitter  `button`

--- client/src/components/NavBar/AuthMenu.tsx
+++ client/src/components/NavBar/AuthMenu.tsx
@@ -6,7 +6,6 @@ const AuthMenu = (): JSX.Element => {
   const { buttonContainer } = useStyles();
   return (
     <Box className={buttonContainer}>
-      <CustomButton linkTo={'/'} btnText={'Become a sitter'} style={'sitter'} />
       <CustomButton linkTo={'/signin'} btnText={'Login'} style={'login'} />
       <CustomButton linkTo={'/signup'} btnText={'Signup'} style={'signup'} />
     </Box>
