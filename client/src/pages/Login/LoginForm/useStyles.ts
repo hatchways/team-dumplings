d01bb4135b97507fb49d17664e8d2b9f5d2bec09
Fetah .OK <32603079+fetahokey@users.noreply.github.com>
2021-10-20 16:54:53 +0100

    remove comment

--- client/src/pages/Login/LoginForm/useStyles.ts
+++ client/src/pages/Login/LoginForm/useStyles.ts
@@ -28,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.common.white,
     border: '1px solid #ced4da',
     fontSize: 16,
-    //width: '80%',
     height: '100%',
     padding: theme.spacing(2),
     marginTop: 0,
