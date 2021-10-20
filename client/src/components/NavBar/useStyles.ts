9f3accebb1dd72d1d0deca00fef10e06cacf4b02
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-20 08:08:16 +0100

    delete unused css class `buttonContainer`

--- client/src/components/NavBar/useStyles.ts
+++ client/src/components/NavBar/useStyles.ts
@@ -34,9 +34,68 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: theme.palette.success.main,
     },
   },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+  button: {
+    fontSize: 12,
+    fontWeight: 600,
+    color: theme.palette.common.white,
+    backgroundColor: '#f04040',
+    border: '1px solid #f04040',
+    padding: theme.spacing(2, 4, 2, 4),
+    '&:hover': {
+      backgroundColor: 'transparent',
+      color: '#f04040',
+    },
+
+    '&.mySitters': {
+      padding: theme.spacing(1, 4, 1, 4),
+      backgroundColor: 'transparent',
+      border: 'none',
+      color: '#f04040',
+      [theme.breakpoints.down('sm')]: {
+        padding: theme.spacing(1),
+      },
+    },
+
+    '&.messages': {
+      padding: theme.spacing(1, 4, 1, 4),
+      backgroundColor: 'transparent',
+      border: 'none',
+      color: theme.palette.common.black,
+      [theme.breakpoints.down('sm')]: {
+        padding: theme.spacing(1),
+      },
+    },
+
+    '&.sitter': {
+      padding: theme.spacing(1, 4, 1, 4),
+      backgroundColor: 'transparent',
+      border: 'none',
+      textDecorationLine: 'underline',
+      textDecorationStyle: 'solid',
+      textDecorationColor: theme.palette.common.black,
+      color: theme.palette.common.black,
+      [theme.breakpoints.down('sm')]: {
+        padding: theme.spacing(1),
+      },
+    },
+    '&.login': {
+      padding: theme.spacing(1, 4, 1, 4),
+      color: '#f04040',
+      fontSize: 12,
+      fontWeight: 600,
+      backgroundColor: 'transparent',
+      border: '1px solid #f04040',
+      '&:hover': {
+        backgroundColor: '#f04040',
+        color: theme.palette.common.white,
+      },
+      [theme.breakpoints.down('sm')]: {
+        color: '#f04040',
+      },
+    },
+    '&.signup': {
+      padding: theme.spacing(1, 4, 1, 4),
+    },
   },
 }));
 
