6e4a802c2897f638ed4d3f7a2cec5454fb5884d2
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-21 00:15:40 +0100

    update `/signin` to `/login` && remove `Become a sitter` button

--- client/src/components/NavBar/AuthMenu.tsx
+++ client/src/components/NavBar/AuthMenu.tsx
@@ -4,8 +4,7 @@ import { CustomButton } from '../Button/CustomButton';
 const AuthMenu = (): JSX.Element => {
   return (
     <Box display="flex" justifyContent="flex-end">
-      <CustomButton linkTo={'/'} btnText={'Become a sitter'} style={'sitter'} />
-      <CustomButton linkTo={'/signin'} btnText={'Login'} style={'login'} />
+      <CustomButton linkTo={'/login'} btnText={'Login'} style={'login'} />
       <CustomButton linkTo={'/signup'} btnText={'Signup'} style={'signup'} />
     </Box>
   );
