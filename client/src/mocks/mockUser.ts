ae6ca785be960c5722c904b9196763a405be42ea
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-23 12:50:58 +0100

    update the `mockusers` with role attribute

--- client/src/mocks/mockUser.ts
+++ client/src/mocks/mockUser.ts
@@ -3,19 +3,23 @@ import { User } from '../interface/User';
 const mockLoggedInUser: User = {
   email: 'mockLoggedInUser@gmail.com',
   username: 'mock LoggedIn user',
+  role: 'owner',
 };
 
 const mockOtherUser1: User = {
   username: 'Mock test user 1',
   email: 'mockTestUser1@gmail.com',
+  role: 'sitter',
 };
 const mockOtherUser2: User = {
   username: 'Mock test user 2',
   email: 'mockTestUser2@gmail.com',
+  role: 'owner',
 };
 const mockOtherUser3: User = {
   username: 'Mock test user 3',
   email: 'mockTestUser3@gmail.com',
+  role: 'sitter',
 };
 
 const mockOtherUsers: User[] = [mockOtherUser1, mockOtherUser2, mockOtherUser3];
