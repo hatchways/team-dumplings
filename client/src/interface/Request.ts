5d864282a8c2b6811adccf40cf8e7040ebecf326
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-25 02:06:27 +0100

    new interface `SittingRequest` to handle request display/update

--- client/src/interface/Request.ts
+++ client/src/interface/Request.ts
@@ -6,14 +6,57 @@ export interface Sitter {
 export interface Request {
   _id: string;
   ownerId: string;
-  sitterId: { _id: string; username: string };
+  sitterId: any;
   dogId: string;
   start: Date;
   end: Date;
   status: string;
 }
 
+export type requstStatus = 'pending' | 'declined' | 'accepted' | 'paid' | 'progress' | 'done';
+export type dogGender = 'male' | 'female' | 'other';
+export type dogSize = 'xs' | 's' | 'm' | 'l' | 'g';
+export interface RequestProfile {
+  _id: string;
+  firstName?: string;
+  lastName?: string;
+  rate?: number;
+}
+export interface RequestUserAndProfile {
+  _id: string;
+  profile?: RequestProfile;
+}
+export interface SittingRequest {
+  _id: string;
+  status: requstStatus;
+  ownerId: RequestUserAndProfile;
+  sitterId: RequestUserAndProfile;
+  dogId: {
+    _id: string;
+    ownerId: string;
+    neutered: boolean;
+    chipped: boolean;
+    vaccinated: boolean;
+    houseTrained: boolean;
+    friendlyWithDogs: boolean;
+    friendlyWithCats: boolean;
+    friendlyWithKids: boolean;
+    friendlyWithAdults: boolean;
+    name: string;
+    breed: string;
+    size: dogSize;
+    gender: dogGender;
+    yearOfBirth: number;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  start: Date;
+  end: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
 export interface RequestsApiData {
-  requests?: Request[];
+  updatedRequest?: Request;
+  requests?: Request[] | SittingRequest[];
   error?: { message: string };
 }
