be4c2cf7fb81713db7eb2dd5ff3a0260a3eb8418
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-27 00:31:58 +0100

    append  `validateStripeRequest` funciton in the route arg as middleware

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
+const { validateStripeRequest } = require("..//validators/common");
 const { chargeCustomer } = require("../controllers/stripe");
 
-router.route("/:id/pay").post(protect, chargeCustomer);
+router.route("/:id/pay").post(protect, validateStripeRequest, chargeCustomer);
 module.exports = router;
