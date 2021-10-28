4e71954096a2608284506b4a8d4f85e6162f297e
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-28 22:07:51 +0100

    temporary route to redirect automatically to the `/payment` route

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -25,7 +25,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
   const updateLoginContext = useCallback(
     (data: AuthApiDataSuccess) => {
       setLoggedInUser(data.user);
-      history.push('/messages');
+      history.push('/payment');
     },
     [history],
   );
@@ -46,7 +46,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       await loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
           updateLoginContext(data.success);
-          history.push('/messages');
+          history.push('/payment');
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
