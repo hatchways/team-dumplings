0a6d7c01c7091c786346244e6deba1e5b8acad2a
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-28 06:11:04 +0100

    display the `availability` on the card

--- client/src/pages/Listing/ListingItem.tsx
+++ client/src/pages/Listing/ListingItem.tsx
@@ -4,6 +4,7 @@ import {
   Card,
   CardActionArea,
   CardContent,
+  Chip,
   Divider,
   Grid,
   IconButton,
@@ -11,11 +12,25 @@ import {
 } from '@material-ui/core';
 import RoomIcon from '@material-ui/icons/Room';
 import { Rating } from '@material-ui/lab';
+import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import useStyles from './useStyles';
 
 export type rate = 1 | 2 | 3 | 4 | 5 | 0;
 
+interface dayAvailabilitySchema {
+  startTime: number;
+  endTime: number;
+}
+interface Availability {
+  0: [dayAvailabilitySchema];
+  1: [dayAvailabilitySchema];
+  2: [dayAvailabilitySchema];
+  3: [dayAvailabilitySchema];
+  4: [dayAvailabilitySchema];
+  5: [dayAvailabilitySchema];
+  6: [dayAvailabilitySchema];
+}
 interface Props {
   image?: string;
   fullName: string;
@@ -24,9 +39,23 @@ interface Props {
   description?: string;
   location: string;
   price: number;
+  availability?: Availability;
 }
 
-export const ListingItem = ({ image, fullName, rating, slogan, description, location, price }: Props): JSX.Element => {
+const dayOfWeekAsString = (dayIndex: number): string => {
+  return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][dayIndex];
+};
+
+export const ListingItem = ({
+  image,
+  fullName,
+  rating,
+  slogan,
+  description,
+  location,
+  price,
+  availability,
+}: Props): JSX.Element => {
   const {
     itemContainer,
     largeAvatar,
@@ -36,11 +65,12 @@ export const ListingItem = ({ image, fullName, rating, slogan, description, loca
     priceStyle,
     iconStyle,
     avatarButton,
+    cardStyle,
   } = useStyles();
   return (
     <Grid item xs={12} sm={6} md={4} xl={3} className={itemContainer}>
-      <Card component={Box}>
-        <Link component={CardActionArea} to={'linkToProfile'}>
+      <Card component={Box} className={cardStyle}>
+        <Link component={CardActionArea} to={'/linkToProfile'}>
           <CardContent style={{ padding: 0 }}>
             <Box display={'flex'} flexDirection="column" alignItems="center" justifyContent="center" pt={3} pb={3}>
               <IconButton className={avatarButton}>
@@ -49,11 +79,56 @@ export const ListingItem = ({ image, fullName, rating, slogan, description, loca
               <Typography variant="h3" className={fullNameStyle}>
                 {fullName}
               </Typography>
-              <Typography variant="subtitle1">{slogan}</Typography>
+
               <Rating name="read-only" value={rating as number} readOnly />
               <Typography className={descriptionStyle} align="center">
                 {description}
               </Typography>
+              <Box pt={2}>
+                <Divider light />
+                <Chip
+                  variant={_.isEmpty(availability?.[0]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(0)}
+                />
+                <Chip
+                  variant={_.isEmpty(availability?.[1]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(1)}
+                />
+                <Chip
+                  variant={_.isEmpty(availability?.[2]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(2)}
+                />
+                <Chip
+                  variant={_.isEmpty(availability?.[3]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(3)}
+                />
+                <Chip
+                  variant={_.isEmpty(availability?.[4]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(4)}
+                />
+                <Chip
+                  variant={_.isEmpty(availability?.[5]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(5)}
+                />
+                <Chip
+                  variant={_.isEmpty(availability?.[6]) ? 'outlined' : 'default'}
+                  color="primary"
+                  size="small"
+                  label={dayOfWeekAsString(6)}
+                />
+              </Box>
             </Box>
             <Divider light />
             <Box flex={2} display={'flex'} alignContent="space-between" pt={2} pb={2} pl={4} pr={4}>
