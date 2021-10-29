8a79c1a028525b44c7cd11f4c6ce4e0b1d0bb4e3
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-29 02:30:27 +0100

    update the `stripe` route wiht the appropriate validation functions

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const { validateStripeRequest } = require("..//validators/common");
-const { chargeCustomer } = require("../controllers/stripe");
+const { validateId, validateStripeRequest } = require("../validators/common");
+const { createPaymentIntent } = require("../controllers/stripe");
 
-router.route("/:id/pay").post(protect, validateStripeRequest, chargeCustomer);
+router
+  .route("/:id/pay")
+  .post([protect, validateId, validateStripeRequest], createPaymentIntent);
+
 module.exports = router;
