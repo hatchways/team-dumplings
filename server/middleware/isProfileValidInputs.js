c22927cdf1596c5e706e1a56e11f9964c0f639bc
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-30 23:54:26 +0100

    update `isProfileValidInputs` to handle the customerId

--- server/middleware/isProfileValidInputs.js
+++ server/middleware/isProfileValidInputs.js
@@ -1,14 +1,26 @@
 const isProfileValidInputs = (req, res, next) => {
-    const inputs = Object.keys(req.body);
-    const allowedInputs = ['firstName', 'lastName', 'gender', 'phoneNumber', 'address', 'availability', 'description', 'birthDate'];
-    const isValidOperation = inputs.every((input) => allowedInputs.includes(input));
+  const inputs = Object.keys(req.body);
+  const allowedInputs = [
+    "firstName",
+    "lastName",
+    "gender",
+    "phoneNumber",
+    "address",
+    "availability",
+    "description",
+    "birthDate",
+    "customerId",
+  ];
+  const isValidOperation = inputs.every((input) =>
+    allowedInputs.includes(input)
+  );
 
-    if (!isValidOperation) {
-        res.status(400);
-        throw new Error('Invalid Update');
-    } 
+  if (!isValidOperation) {
+    res.status(400);
+    throw new Error("Invalid Update");
+  }
 
-    next();
-}
+  next();
+};
 
 module.exports = isProfileValidInputs;
