6130824b9bacdaa7726989feadcd05ba64aa40aa
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-31 00:02:23 +0100

    `payment` page styles

--- client/src/pages/Payment/useStyles.ts
+++ client/src/pages/Payment/useStyles.ts
@@ -13,8 +13,34 @@ const useStyles = makeStyles((theme) => ({
 
   title: {
     color: theme.palette.common.black,
-    fontWeight: 400,
+    fontWeight: 500,
     textAlign: 'center',
+  },
+  subTitle: {
+    color: theme.palette.grey[600],
+    fontWeight: 300,
+    textAlign: 'left',
+  },
+  cardBrandLogo: {
+    width: '85px',
+    height: '42px',
+    objectFit: 'contain',
+  },
+  creditCardNumber: {
+    fontSize: 14,
+    fontWeight: 600,
+    wordSpacing: 4,
+    color: theme.palette.common.black,
+  },
+  creditCardExpDate: {
+    fontWeight: 500,
+    paddingBottom: theme.spacing(2),
+  },
+  creditCardHolder: {
+    fontSize: 16,
+    fontWeight: 600,
+    color: theme.palette.common.black,
+    textTransform: 'uppercase',
   },
   sideBarMenuItem: {
     padding: theme.spacing(1),
@@ -33,6 +59,41 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.grey[700],
       fontWeight: 400,
     },
+  },
+  inputs: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 14,
+    width: '80%',
+    height: '100%',
+    padding: theme.spacing(2),
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+  creditCardInputStyle: {
+    padding: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    width: '85%',
+    height: '28px',
+    fontSize: 12,
+    '& .credit-card-input': {},
+  },
+  label: {
+    fontWeight: 800,
+    fontSize: 12,
+    color: theme.palette.common.black,
+    textTransform: 'uppercase',
+    marginTop: theme.spacing(1),
+  },
+  CreditCardInputClass: {
+    color: 'red !important',
+    fontSize: '10px   !important',
+    //display: 'none',
   },
 }));
 
