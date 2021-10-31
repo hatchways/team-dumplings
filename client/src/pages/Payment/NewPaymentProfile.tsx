cd616301d36711b7224f15d5aa6c0e3d7543917b
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-31 17:47:59 +0000

    `NewPaymentProfile` JSX element, Dialog to add new Payment method

--- client/src/pages/Payment/NewPaymentProfile.tsx
+++ client/src/pages/Payment/NewPaymentProfile.tsx
@@ -10,31 +10,29 @@ import {
   TextField,
 } from '@material-ui/core';
 import clsx from 'clsx';
-import { Formik, FormikHelpers } from 'formik';
-import { useCallback, useEffect, useState } from 'react';
-import CreditCardInput from 'react-credit-card-input';
 import * as Yup from 'yup';
+import CreditCardInput from 'react-credit-card-input';
+import { Formik, FormikHelpers } from 'formik';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../../context/useAuthContext';
 import { useSnackBar } from '../../context/useSnackbarContext';
 import { createCustomer, createPaymentMethod } from '../../helpers/APICalls/payment';
 import { editProfile, getProfile } from '../../helpers/APICalls/profile';
-import { Profile, ProfileApiDataSuccess } from '../../interface/Profile';
+import { ProfileApiDataSuccess } from '../../interface/Profile';
 import useStyles from './useStyles';
-import { CardElement, useStripe, useElements, Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
 
 interface Props {
   fullWidth: boolean;
   open: boolean;
+  // eslint-disable-next-line
   handleClose: any;
+  // eslint-disable-next-line
   callBackAction: any;
 }
 
 const NewPaymentProfile = ({ fullWidth, open, handleClose, callBackAction }: Props): JSX.Element => {
-  const stripe = useStripe();
-  const elements = useElements();
-
   const { inputs, creditCardInputStyle, label, CreditCardInputClass } = useStyles();
+  // eslint-disable-next-line
   const [loading, setLoading] = useState<boolean>(false);
   const { loggedInUser } = useAuth();
   const { updateSnackBarMessage } = useSnackBar();
@@ -117,7 +115,9 @@ const NewPaymentProfile = ({ fullWidth, open, handleClose, callBackAction }: Pro
   const handleSubmit = (
     {
       fullName,
+      // eslint-disable-next-line
       email,
+      // eslint-disable-next-line
       phoneNumber,
       creditCardNumber,
       cvcField,
@@ -131,6 +131,7 @@ const NewPaymentProfile = ({ fullWidth, open, handleClose, callBackAction }: Pro
       expDate: string;
     },
     {
+      // eslint-disable-next-line
       setSubmitting,
     }: FormikHelpers<{
       fullName: string;
@@ -150,20 +151,6 @@ const NewPaymentProfile = ({ fullWidth, open, handleClose, callBackAction }: Pro
     });
 
     handleClose();
-    /*stripe &&
-      stripe
-        .confirmCardPayment('pi_3JqJ5aG4Np2TPeFN0Ha60YaZ_secret_j0BCGPUXTkdpC4Zh2w580VoXB', {
-          payment_method: 'card_1JqI39G4Np2TPeFNf17UkxVy',
-        })
-        .then((result: any) => {
-          // Handle result.error or result.paymentIntent
-          if (result.paymentIntent) {
-            console.log(`result.paymentIntent:  ${JSON.stringify(result.paymentIntent)}`);
-          } else {
-            console.log(`result.error:  ${JSON.stringify(result.error)}`);
-          }
-        }); */
-    // create a token and use this token to create source for the current customer :)
   };
 
   return (
@@ -171,7 +158,7 @@ const NewPaymentProfile = ({ fullWidth, open, handleClose, callBackAction }: Pro
       <Dialog fullWidth={fullWidth} maxWidth={'sm'} open={open} onClose={handleClose}>
         <DialogTitle id="newPaymentProfile">New payment profile</DialogTitle>
         <DialogContent>
-          <DialogContentText>You can set up to two profiles.</DialogContentText>
+          <DialogContentText>You can set up to two (02) payment profiles.</DialogContentText>
           <Formik
             initialValues={{
               fullName: fullName,
@@ -200,116 +187,118 @@ const NewPaymentProfile = ({ fullWidth, open, handleClose, callBackAction }: Pro
             })}
             onSubmit={handleSubmit}
           >
-            {({ handleSubmit, handleChange, setFieldValue, values, touched, errors, isSubmitting }) => (
-              <form onSubmit={handleSubmit} noValidate>
-                <Grid container spacing={1}>
-                  <Grid item xs={12} md={6}>
-                    <InputLabel htmlFor="fullName" className={label}>
-                      Full name
-                    </InputLabel>
-                    <TextField
-                      id="fullName"
-                      fullWidth
-                      margin="normal"
-                      InputProps={{
-                        classes: { input: inputs },
-                        disableUnderline: true,
-                      }}
-                      name="fullName"
-                      autoComplete="fullName"
-                      placeholder="Full name"
-                      autoFocus
-                      helperText={touched.fullName ? errors.fullName : ''}
-                      error={touched.fullName && Boolean(errors.fullName)}
-                      value={values.fullName}
-                      onChange={handleChange}
-                    />
-                    <InputLabel htmlFor="email" className={label}>
-                      Email
-                    </InputLabel>
+            {
+              // eslint-disable-next-line
+              ({ handleSubmit, handleChange, setFieldValue, values, touched, errors, isSubmitting }) => (
+                <form onSubmit={handleSubmit} noValidate>
+                  <Grid container spacing={1}>
+                    <Grid item xs={12} md={6}>
+                      <InputLabel htmlFor="fullName" className={label}>
+                        Full name
+                      </InputLabel>
+                      <TextField
+                        id="fullName"
+                        fullWidth
+                        margin="normal"
+                        InputProps={{
+                          classes: { input: inputs },
+                          disableUnderline: true,
+                        }}
+                        name="fullName"
+                        autoComplete="fullName"
+                        placeholder="Full name"
+                        autoFocus
+                        helperText={touched.fullName ? errors.fullName : ''}
+                        error={touched.fullName && Boolean(errors.fullName)}
+                        value={values.fullName}
+                        onChange={handleChange}
+                      />
+                      <InputLabel htmlFor="email" className={label}>
+                        Email
+                      </InputLabel>
 
-                    <TextField
-                      id="email"
-                      fullWidth
-                      margin="normal"
-                      InputProps={{
-                        classes: { input: inputs },
-                        disableUnderline: true,
-                      }}
-                      name="email"
-                      autoComplete="email"
-                      placeholder="email@domain.com"
-                      helperText={touched.email ? errors.email : ''}
-                      error={touched.email && Boolean(errors.email)}
-                      value={values.email}
-                      onChange={handleChange}
-                    />
+                      <TextField
+                        id="email"
+                        fullWidth
+                        margin="normal"
+                        InputProps={{
+                          classes: { input: inputs },
+                          disableUnderline: true,
+                        }}
+                        name="email"
+                        autoComplete="email"
+                        placeholder="email@domain.com"
+                        helperText={touched.email ? errors.email : ''}
+                        error={touched.email && Boolean(errors.email)}
+                        value={values.email}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <InputLabel htmlFor="phoneNumber" className={label}>
+                        Phone number
+                      </InputLabel>
+                      <TextField
+                        id="phoneNumber"
+                        fullWidth
+                        margin="normal"
+                        InputProps={{
+                          classes: { input: inputs },
+                          disableUnderline: true,
+                        }}
+                        autoComplete="phone"
+                        placeholder="+999 999 9999"
+                        helperText={touched.phoneNumber ? errors.phoneNumber : ''}
+                        error={touched.phoneNumber && Boolean(errors.phoneNumber)}
+                        value={values.phoneNumber}
+                        onChange={handleChange}
+                      />
+
+                      <InputLabel htmlFor="cardNumber" className={label}>
+                        Card number
+                      </InputLabel>
+                      <CreditCardInput
+                        cardNumberInputProps={{
+                          name: 'creditCardNumber',
+                          id: 'creditCardNumber',
+                          value: values.creditCardNumber,
+                          onChange: handleChange,
+                          error: touched.creditCardNumber && Boolean(errors.creditCardNumber),
+                          helperText: touched.creditCardNumber ? errors.creditCardNumber : '',
+                          autoFocus: true,
+                        }}
+                        cardExpiryInputProps={{
+                          name: 'expDate',
+                          id: 'expDate',
+                          value: values.expDate,
+                          onChange: handleChange,
+                          error: touched.expDate && Boolean(errors.expDate),
+                          helperText: touched.expDate ? errors.expDate : '',
+                        }}
+                        cardCVCInputProps={{
+                          name: 'cvcField',
+                          id: 'cvcField',
+                          value: values.cvcField,
+                          onChange: handleChange,
+                          error: touched.cvcField && Boolean(errors.cvcField),
+                          helperText: touched.cvcField ? errors.cvcField : '',
+                        }}
+                        fieldClassName={clsx(inputs, creditCardInputStyle)}
+                        dangerTextClassName={CreditCardInputClass}
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12} md={6}>
-                    <InputLabel htmlFor="phoneNumber" className={label}>
-                      Phone number
-                    </InputLabel>
-                    <TextField
-                      id="phoneNumber"
-                      fullWidth
-                      margin="normal"
-                      InputProps={{
-                        classes: { input: inputs },
-                        disableUnderline: true,
-                      }}
-                      autoComplete="phone"
-                      placeholder="+123 546 5654"
-                      helperText={touched.phoneNumber ? errors.phoneNumber : ''}
-                      error={touched.phoneNumber && Boolean(errors.phoneNumber)}
-                      value={values.phoneNumber}
-                      onChange={handleChange}
-                    />
-
-                    <InputLabel htmlFor="cardNumber" className={label}>
-                      Card number
-                    </InputLabel>
-                    <CreditCardInput
-                      cardNumberInputProps={{
-                        name: 'creditCardNumber',
-                        id: 'creditCardNumber',
-                        value: values.creditCardNumber,
-                        onChange: handleChange,
-                        error: touched.creditCardNumber && Boolean(errors.creditCardNumber),
-                        helperText: touched.creditCardNumber ? errors.creditCardNumber : '',
-                        autoFocus: true,
-                      }}
-                      cardExpiryInputProps={{
-                        name: 'expDate',
-                        id: 'expDate',
-                        value: values.expDate,
-                        onChange: handleChange,
-                        error: touched.expDate && Boolean(errors.expDate),
-                        helperText: touched.expDate ? errors.expDate : '',
-                      }}
-                      cardCVCInputProps={{
-                        name: 'cvcField',
-                        id: 'cvcField',
-                        value: values.cvcField,
-                        onChange: handleChange,
-                        error: touched.cvcField && Boolean(errors.cvcField),
-                        helperText: touched.cvcField ? errors.cvcField : '',
-                      }}
-                      fieldClassName={clsx(inputs, creditCardInputStyle)}
-                      dangerTextClassName={CreditCardInputClass}
-                    />
-                  </Grid>
-                </Grid>
-                <DialogActions>
-                  <Button onClick={handleClose} color="primary">
-                    Close
-                  </Button>
-
-                  <Button type="submit" color="primary">
-                    Save
-                  </Button>
-                </DialogActions>
-              </form>
-            )}
+                  <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                      Close
+                    </Button>
+                    <Button type="submit" color="primary">
+                      Save
+                    </Button>
+                  </DialogActions>
+                </form>
+              )
+            }
           </Formik>
         </DialogContent>
       </Dialog>
