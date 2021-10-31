f8c3a20f0890708528c86c676cc5737f56de8de6
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-10-31 20:49:44 +0000

    refactor `listProfiles` function

--- client/src/helpers/APICalls/listProfiles.ts
+++ client/src/helpers/APICalls/listProfiles.ts
@@ -1,17 +1,22 @@
 import { FetchOptions } from '../../interface/FetchOptions';
 import { ProfileApiData } from '../../interface/Profile';
 
-export async function listProfiles(
-  filter: boolean,
-  dateIn: number,
-  dateOff: number,
-  search: string,
-): Promise<ProfileApiData> {
+interface Props {
+  filter: boolean;
+  dateIn: number;
+  dateOff: number;
+  searchString: string;
+}
+
+export async function listProfiles({ filter, dateIn, dateOff, searchString }: Props): Promise<ProfileApiData> {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     credentials: 'include',
   };
-  return await fetch(`/profile?filter=${filter}&dateIn=${dateIn}&dateOff=${dateOff}&search=${search}`, fetchOptions)
+  return await fetch(
+    `/profile?filter=${filter}&dateIn=${dateIn}&dateOff=${dateOff}&search=${searchString}`,
+    fetchOptions,
+  )
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
