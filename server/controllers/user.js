028954c246772d5f101c944bd9b56e380a903fc7
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-03 22:47:56 +0000

    update `searchUsers` to return only users with profile

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,44 @@
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
-
+/**
+ * 
+ *   const profiles = await Profile.find()
+    .populate({
+      path: "user",
+      select: { password: 0 },
+      match: {
+        role: "sitter",
+      },
+      options: { retainNullValues: false },
+      transform: (doc, id) => (doc == null ? null : doc),
+    })
+    .exec()
+    .then((profiles) => {
+      return profiles.filter((profile) => {
+        if (filter) {
+          if (profile.user) {
+            if (
+              !_.isEmpty(profile.availability[dateIn]) &&
+              !_.isEmpty(profile.availability[dateOff])
+            ) {
+              const searchTarget = [
+                profile.firstName.toLowerCase(),
+                profile.lastName.toLowerCase(),
+                profile.address.toLowerCase(),
+              ];
+              const result = searchTarget.find((searchTargetItem) =>
+                searchTargetItem.includes(search.toLowerCase())
+              );
+              return !!result;
+            }
+          }
+          return false;
+        } else {
+          return profile.user != null;
+        }
+      });
+    });
+ */
 // @route POST /users
 // @desc Search for users
 // @access Private
@@ -11,9 +49,14 @@ exports.searchUsers = asyncHandler(async (req, res, next) => {
   if (searchString) {
     users = await User.find({
       username: { $regex: searchString, $options: "i" },
-    });
+    })
+      .select(" -password  ")
+      .populate("profile", "firstName lastName")
+      .exec()
+      .then((result) => {
+        return result.filter((userWithProfile) => !!userWithProfile.profile);
+      });
   }
-
   if (!users) {
     res.status(404);
     throw new Error("No users found in search");
