791b0e04d16b0e1cd18df34599e77fc550e7ee15
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-03 22:46:59 +0000

    add `latestMessage` field on `conversationSchema`

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const Profile = require("./Profile");
 
 const conversationSchema = new mongoose.Schema(
   {
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: "Profile" }],
+    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
   },
   {
     timestamps: true,
