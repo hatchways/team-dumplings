f3105f7071a5814ad43eb6de59a5094490148a7c
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-03 22:50:51 +0000

    `Messages` integration with BE

--- client/src/pages/Messages/Messages.tsx
+++ client/src/pages/Messages/Messages.tsx
@@ -1,21 +1,40 @@
 import { Grid } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
-import Search from '../../components/Search/Search';
+import { useAuth } from '../../context/useAuthContext';
+import { useConversation } from '../../context/useConversationContext';
 import ChatBox from './ChatBox';
 import { ChatSideBar } from './ChatSideBar';
 import useStyles from './useStyles';
 
 const Messages = (): JSX.Element => {
+  const [profileId, setProfileId] = useState<string | null>(null);
+
+  const { conversations } = useConversation();
+
   const { root } = useStyles();
+  const { loggedInUser } = useAuth();
+
+  const saveProfileId = (id: string) => {
+    setProfileId(id);
+  };
+
+  useEffect(() => {
+    let active = true;
+    loggedInUser && loggedInUser.profile && saveProfileId(loggedInUser.profile);
+    return () => {
+      active = false;
+    };
+  }, [loggedInUser]);
   return (
     <>
       <NavBar />
       <Grid container className={root} direction="row">
         <Grid container item md={3}>
-          <ChatSideBar />
+          <ChatSideBar conversations={conversations!} profileId={profileId!} />
         </Grid>
         <Grid container item md={9}>
-          <ChatBox />
+          <ChatBox profileId={profileId!} />
         </Grid>
       </Grid>
     </>
