229b35d2fb058b7624b84fec873fa9ee22b283ba
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-03 22:52:32 +0000

    `ChatSideBanner` integration with BE

--- client/src/components/ChatSideBanner/ChatSideBanner.tsx
+++ client/src/components/ChatSideBanner/ChatSideBanner.tsx
@@ -3,7 +3,7 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import useStyles from './useStyles';
-import { User } from '../../interface/User';
+import { User, UserFromSearch } from '../../interface/User';
 import AvatarDisplay from '../AvatarDisplay/AvatarDisplay';
 import Search from '../Search/Search';
 import AuthMenu from '../AuthMenu/AuthMenu';
@@ -17,7 +17,7 @@ const ChatSideBanner = ({ loggedInUser }: Props): JSX.Element => {
   const [search, setSearch] = useState<string>('test');
   const [newChatUser, setNewChatUser] = useState<User | null>(null);
   const classes = useStyles();
-  const [options, setOptions] = useState<User[]>([]);
+  const [options, setOptions] = useState<UserFromSearch[]>([]);
 
   // React.FormEvent<FormControl & FormControlProps>)
   const handleChange = (e: ChangeEvent<HTMLInputElement>, newInputValue: string) => {
