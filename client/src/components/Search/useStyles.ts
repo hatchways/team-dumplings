892967c3dba7150bb69fe4ce0acb3458cbf3296c
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-03 22:52:50 +0000

    update `search` style

--- client/src/components/Search/useStyles.ts
+++ client/src/components/Search/useStyles.ts
@@ -1,7 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const drawerWidth = 240;
-
 const useStyles = makeStyles((theme) => ({
   search: {
     position: 'relative',
@@ -19,7 +17,6 @@ const useStyles = makeStyles((theme) => ({
   },
   searchInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     width: '100%',
     fontWeight: 600,
