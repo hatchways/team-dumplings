0ac78c5b50b5827f13fdde5af7fc7683efaa33b7
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-11 19:05:24 +0000

    :zap: update the auth controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -117,5 +117,5 @@ exports.loadUser = asyncHandler(async (req, res, next) => {
 exports.logoutUser = asyncHandler(async (req, res, next) => {
   res.clearCookie("token");
 
-  res.send("You have successfully logged out");
+  res.json("You have successfully logged out");
 });
