d002eab25c13a9a83bc1bf8ff63085df91fd6300
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 08:41:00 +0000

    :zap: rating api helper

---
+++ client/src/helpers/APICalls/rating.ts
@@ -0,0 +1,26 @@
+import { FetchOptions } from '../../interface/FetchOptions';
+import { Comment, CommentApiDataSuccess } from '../../interface/Rating';
+
+export const createComment = async (comment: Comment): Promise<CommentApiDataSuccess> => {
+  const fetchOptions: FetchOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(comment),
+    credentials: 'include',
+  };
+  return await fetch(`/comments/`, fetchOptions)
+    .then((res) => res.json())
+    .catch((error) => ({ error }));
+};
+
+export async function listComments(profileId: string): Promise<CommentApiDataSuccess> {
+  const fetchOptions: FetchOptions = {
+    method: 'GET',
+    credentials: 'include',
+  };
+  return await fetch(`/comments/${profileId}`, fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({
+      error: { message: 'Unable to connect to server. Please try again' },
+    }));
+}
