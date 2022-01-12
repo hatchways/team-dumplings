e704f100ddd573ae3790441a40edf4b3db5e52e0
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 18:07:34 +0000

    ❌ delete unused `getStats` function

--- client/src/helpers/APICalls/rating.ts
+++ client/src/helpers/APICalls/rating.ts
@@ -26,15 +26,3 @@ export async function listComments(profileId: string, limit: number, skip: numbe
       error: { message: 'Unable to connect to server. Please try again' },
     }));
 }
-
-export async function loadReviewStats(profileId: string): Promise<CommentApiDataSuccess> {
-  const fetchOptions: FetchOptions = {
-    method: 'GET',
-    credentials: 'include',
-  };
-  return await fetch(`/comments/${profileId}`, fetchOptions)
-    .then((res) => res.json())
-    .catch(() => ({
-      error: { message: 'Unable to connect to server. Please try again' },
-    }));
-}
