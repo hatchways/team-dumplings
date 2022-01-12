60860d31c540d734cd413e16d4e3afd68bc676c9
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 11:14:21 +0000

    :tada: new `loadStats` toute

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { loadComments, createComment } = require("../controllers/review");
+const {
+  loadComments,
+  createComment,
+  loadStats,
+} = require("../controllers/review");
 const router = express.Router();
 const protect = require("../middleware/auth");
 const { validateCreateComment } = require("../validators/review");
 
 router.route("/").post(protect, validateCreateComment, createComment);
-router.route("/:id").get(protect, loadComments);
+router.route("/:id").post(protect, loadComments);
+router.route("/:id").get(protect, loadStats);
 
 module.exports = router;
