45583916c6e0896eece54937e239e0ac597d9ff1
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 18:11:06 +0000

    🗑️ remove unused route `/id` load stats <

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const {
-  loadComments,
-  createComment,
-  loadStats,
-} = require("../controllers/review");
+const { loadComments, createComment } = require("../controllers/review");
 const router = express.Router();
 const protect = require("../middleware/auth");
 const { validateCreateComment } = require("../validators/review");
 
 router.route("/").post(protect, validateCreateComment, createComment);
 router.route("/:id").post(protect, loadComments);
-router.route("/:id").get(protect, loadStats);
 
 module.exports = router;
