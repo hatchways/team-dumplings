acabb84f4571277c517f5daa3cca978f1a44de32
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 08:44:19 +0000

    :zap: review controller

---
+++ server/controllers/review.js
@@ -0,0 +1,39 @@
+const expressAsyncHandler = require("express-async-handler");
+const Profile = require("../models/Profile");
+const Rating = require("../models/Rating");
+
+exports.createComment = expressAsyncHandler(async (req, res, next) => {
+  const { text, rating, profile } = req.body;
+  const reviewer = req.user.id;
+  const reviewerProfile = await Profile.findOne({ user: reviewer });
+
+  const newRating = await Rating.create({
+    text,
+    rating,
+    profile,
+    reviewer: reviewerProfile._id,
+  });
+
+  if (newRating) {
+    res.status(201).json({
+      rating: newRating,
+    });
+  } else {
+    res.status(500);
+    throw new Error("Internal Server Error");
+  }
+});
+
+exports.loadComments = expressAsyncHandler(async (req, res, next) => {
+  const profile = req.params.id;
+  Rating.find({ profile })
+    .populate("reviewer")
+    .exec()
+    .then((comments) => {
+      if (comments) {
+        res.status(200).send({
+          ratings: comments,
+        });
+      }
+    });
+});
