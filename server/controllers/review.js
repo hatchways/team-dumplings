0522207aac5efff2b77878137ff44cf4d9c6c044
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 11:14:03 +0000

    :tada: new review controller function LoadStats, update listComment to use paginataion

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -26,13 +26,71 @@ exports.createComment = expressAsyncHandler(async (req, res, next) => {
 
 exports.loadComments = expressAsyncHandler(async (req, res, next) => {
   const profile = req.params.id;
+  const { limit = 5, skip = 0 } = req.body;
   Rating.find({ profile })
     .populate("reviewer")
+    .limit(limit)
+    .skip(skip)
+    .sort({
+      createdAt: "desc",
+    })
     .exec()
     .then((comments) => {
       if (comments) {
         res.status(200).send({
           ratings: comments,
+        });
+      }
+    });
+});
+
+exports.loadStats = expressAsyncHandler(async (req, res, next) => {
+  const profile = req.params.id;
+  Rating.find({ profile })
+    .exec()
+    .then((comments) => {
+      if (comments) {
+        const sumRating = (
+          comments.reduce((a, { rating }) => a + rating, 0) / comments.length
+        ).toFixed(1);
+        const sum1Ratings =
+          (comments.filter((comment) => comment.rating == 1).length * 100) /
+          comments.length;
+        const sum2Ratings =
+          (comments.filter((comment) => comment.rating == 2).length * 100) /
+          comments.length;
+        const sum3Ratings =
+          (comments.filter((comment) => comment.rating == 3).length * 100) /
+          comments.length;
+        const sum4Ratings =
+          (comments.filter((comment) => comment.rating == 4).length * 100) /
+          comments.length;
+        const sum5Ratings =
+          (comments.filter((comment) => comment.rating == 5).length * 100) /
+          comments.length;
+        const total = comments.length;
+        res.status(200).json({
+          stats: {
+            sumRating,
+            sum1Ratings,
+            sum2Ratings,
+            sum3Ratings,
+            sum4Ratings,
+            sum5Ratings,
+            total,
+          },
+        });
+      } else {
+        res.status(200).json({
+          stats: {
+            sumRating: 0,
+            sum1Ratings: 0,
+            sum2Ratings: 0,
+            sum3Ratings: 0,
+            sum4Ratings: 0,
+            sum5Ratings: 0,
+            total: 0,
+          },
         });
       }
     });
