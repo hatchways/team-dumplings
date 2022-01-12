9f0e774f000cdb662519b9d69409a20661ce2309
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-12 11:11:13 +0000

    :tada: update `listComments` and add `loadReviewStats` function

--- client/src/helpers/APICalls/rating.ts
+++ client/src/helpers/APICalls/rating.ts
@@ -13,7 +13,21 @@ export const createComment = async (comment: Comment): Promise<CommentApiDataSuc
     .catch((error) => ({ error }));
 };
 
-export async function listComments(profileId: string): Promise<CommentApiDataSuccess> {
+export async function listComments(profileId: string, limit: number, skip: number): Promise<CommentApiDataSuccess> {
+  const fetchOptions: FetchOptions = {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ limit, skip }),
+  };
+  return await fetch(`/comments/${profileId}`, fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({
+      error: { message: 'Unable to connect to server. Please try again' },
+    }));
+}
+
+export async function loadReviewStats(profileId: string): Promise<CommentApiDataSuccess> {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     credentials: 'include',
