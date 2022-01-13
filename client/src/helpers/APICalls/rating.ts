a5869195a7a245fdd0b10a6e8231703c5f8f577a
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-13 19:19:45 +0000

    :tada: new api func's `isLiked` `likeCommnet`

--- client/src/helpers/APICalls/rating.ts
+++ client/src/helpers/APICalls/rating.ts
@@ -13,6 +13,28 @@ export const createComment = async (comment: Comment): Promise<CommentApiDataSuc
     .catch((error) => ({ error }));
 };
 
+export const isLikedComment = async (commentId: string): Promise<CommentApiDataSuccess> => {
+  const fetchOptions: FetchOptions = {
+    method: 'GET',
+    credentials: 'include',
+  };
+  return await fetch(`/comments/like/${commentId}`, fetchOptions)
+    .then((res) => res.json())
+    .catch((error) => ({ error }));
+};
+
+export const likeComment = async (commentId: string, val: number): Promise<CommentApiDataSuccess> => {
+  const fetchOptions: FetchOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ commentId, val }),
+    credentials: 'include',
+  };
+  return await fetch(`/comments/like`, fetchOptions)
+    .then((res) => res.json())
+    .catch((error) => ({ error }));
+};
+
 export async function listComments(profileId: string, limit: number, skip: number): Promise<CommentApiDataSuccess> {
   const fetchOptions: FetchOptions = {
     method: 'POST',
