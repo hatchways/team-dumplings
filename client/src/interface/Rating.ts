edc6580fdb205ec422a08b80821ae99c688383ac
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-13 19:20:07 +0000

    :tada:  update `Rating` interface

--- client/src/interface/Rating.ts
+++ client/src/interface/Rating.ts
@@ -6,12 +6,19 @@ export interface Comment {
   rating: number;
   profile: string;
   reviewer?: Profile;
+  likes?: number;
   createdAt?: string;
   updatedAt?: string;
+}
+
+export interface CommentAndReaction {
+  rating: Comment;
+  like: boolean;
 }
 
 export interface CommentApiDataSuccess {
   rating?: Comment;
   ratings?: Comment[];
+  commentAndReaction?: CommentAndReaction;
   error?: string;
 }
