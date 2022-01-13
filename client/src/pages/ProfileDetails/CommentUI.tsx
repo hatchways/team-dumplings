cdf43c2fd228fa1dd2c53f658e9812d193077599
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-13 19:20:34 +0000

    :tada: update `CommentUI` to use like func's

--- client/src/pages/ProfileDetails/CommentUI.tsx
+++ client/src/pages/ProfileDetails/CommentUI.tsx
@@ -3,6 +3,10 @@ import { Rating } from '@material-ui/lab';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import moment from 'moment';
+import useStyles from './style/useStyles';
+import { isLikedComment, likeComment } from '../../helpers/APICalls/rating';
+import { useSnackBar } from '../../context/useSnackbarContext';
+import { useEffect, useState } from 'react';
 
 interface Props {
   rating: number;
@@ -10,9 +14,47 @@ interface Props {
   createdAt: string;
   firstName: string;
   lastName: string;
+  likes: number;
+  id: string;
 }
 
-const Comment = ({ rating, comment, createdAt, firstName, lastName }: Props): JSX.Element => {
+const Comment = ({ rating, comment, createdAt, firstName, lastName, likes, id }: Props): JSX.Element => {
+  const classes = useStyles();
+  const [likesCount, setLikesCount] = useState(likes);
+  const [isLiked, setIsLiked] = useState(false);
+  const [loading, isLoading] = useState(true);
+  const { updateSnackBarMessage } = useSnackBar();
+
+  const saveIsLikedComment = (like: boolean) => {
+    setIsLiked(like);
+  };
+
+  const handleLikeComment = () => {
+    likeComment(id, isLiked ? -1 : +1)
+      .then((res) => {
+        if (res.rating) {
+          isLoading(true);
+          updateSnackBarMessage('Like ❤️');
+        }
+      })
+      .catch((error) => {
+        updateSnackBarMessage(error);
+      });
+  };
+  useEffect(() => {
+    let effect = true;
+    if (loading) {
+      isLikedComment(id).then((res) => {
+        saveIsLikedComment(res.commentAndReaction?.like || false);
+        setLikesCount(res?.commentAndReaction?.rating?.likes || 0);
+      });
+      isLoading(false);
+    }
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      effect = false;
+    };
+  }, [loading, id]);
   return (
     <>
       <Box>
@@ -27,14 +69,15 @@ const Comment = ({ rating, comment, createdAt, firstName, lastName }: Props): JS
           <CardHeader
             avatar={<Avatar aria-label="avatar">{`${firstName.slice(0, 1)}${lastName.slice(0, 1)}`}</Avatar>}
             action={
-              <>
-                <IconButton aria-label="likes">
-                  <ThumbUpIcon />
+              <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'}>
+                {Boolean(likesCount) && <Typography className={classes.likes}>{likesCount}</Typography>}
+                <IconButton aria-label="likes" onClick={handleLikeComment}>
+                  <ThumbUpIcon className={isLiked ? classes.redlikeIcon : ''} />
                 </IconButton>
                 <IconButton aria-label="settings">
                   <MoreVertIcon />
                 </IconButton>
-              </>
+              </Box>
             }
             title={`${firstName}  ${lastName}`}
             subheader={
