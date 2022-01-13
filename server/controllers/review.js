05886e50ae70dd2e2bc48a9eac2cff76e89aacdb
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-13 19:18:20 +0000

    :tada: add new `review` controller's Like functionality

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,6 +1,67 @@
 const expressAsyncHandler = require("express-async-handler");
+const CommentLikes = require("../models/CommentLikes");
 const Profile = require("../models/Profile");
 const Rating = require("../models/Rating");
+const ObjectId = require("mongoose").Types.ObjectId;
+
+exports.isLikedComment = expressAsyncHandler(async (req, res, next) => {
+  const reviewer = req.user.id;
+  const commentId = req.params.id;
+  const reviewerProfile = await Profile.findOne({ user: reviewer });
+
+  const isLikedComment_ = await CommentLikes.findOne({
+    comment: ObjectId(commentId),
+    profile: ObjectId(reviewerProfile._id),
+  });
+
+  const likedComment = await Rating.findById(commentId);
+
+  if (isLikedComment_) {
+    res.status(200).json({
+      commentAndReaction: { like: true, rating: likedComment },
+    });
+  } else {
+    res.status(200).json({
+      commentAndReaction: { like: false, rating: likedComment },
+    });
+  }
+});
+
+exports.likeComment = expressAsyncHandler(async (req, res, next) => {
+  const reviewer = req.user.id;
+  const { commentId, val } = req.body;
+  const reviewerProfile = await Profile.findOne({ user: reviewer });
+  const updatedRating = await Rating.findOneAndUpdate(
+    { _id: ObjectId(commentId) },
+    {
+      $inc: {
+        likes: val,
+      },
+    }
+  );
+  if (updatedRating) {
+    if (val > 0) {
+      await CommentLikes.create({
+        comment: commentId,
+        profile: reviewerProfile._id,
+      });
+    } else {
+      try {
+        await CommentLikes.findOneAndDelete({
+          comment: ObjectId(commentId),
+          profile: ObjectId(reviewerProfile._id),
+        });
+      } catch (error) {
+        res.status(500);
+        throw new Error("Internal Server Error");
+      }
+    }
+    res.status(200).json({ rating: updatedRating });
+  } else {
+    res.status(500);
+    throw new Error("Internal Server Error");
+  }
+});
 
 exports.createComment = expressAsyncHandler(async (req, res, next) => {
   const { text, rating, profile } = req.body;
